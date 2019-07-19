93016881b91cc0265870803e1d8fba905b1703a5
rajendram <rajendra@mitrmedia.com>
2019-07-19 15:08:08 +0530

    script added

---
+++ deleteFolderWithName.ps1
@@ -0,0 +1,15 @@
+﻿# Script to delete folder with particular name.
+
+$oldFolder=Get-ChildItem -Recurse | Where-Object {$_.Name -like 'old'}
+
+foreach ($old in $oldFolder){
+    $item = $old.FullName
+    Write-Output $item
+    Remove-Item $item -Force -Recurse -ErrorAction SilentlyContinue
+}
+
+
+#### PowerShell Copy Item and exclude extensions. ####
+
+#$exclude = @('*.class','*.swf','*.xml','*.trec','*.camproj')
+#Copy-Item .\Political_Science\ -Recurse -Verbose -Exclude $exclude -Destination ..\Political_Science_new\
